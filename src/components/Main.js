df57fab961ce4a8cee054fa2523d333a56eec024
Anna Botsula <botsula@MacBook-AB.local>
2022-10-17 16:43:54 +0300

    Add help button.

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ import SceneGraph from './scenegraph/SceneGraph';
 import TransformToolbar from './viewport/TransformToolbar';
 // import ViewportHUD from "./viewport/ViewportHUD";
 import { injectCSS } from '../lib/utils';
+import { HelpButton } from './components';
 
 THREE.ImageUtils.crossOrigin = '';
 
@@ -197,6 +198,10 @@ export default class Main extends Component {
           selectedTexture={this.state.selectedTexture}
           onClose={this.onModalTextureOnClose}
         />
+
+        <div id="help">
+          <HelpButton />
+        </div>
       </div>
     );
   }
