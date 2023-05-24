d86b3d7f52b64315357696ad31b54fd7b236ffa0
Ihor Dubas <ihordubas@Ihors-MacBook-Pro.local>
2023-05-24 15:56:09 +0300

    Update styles for below the fold

--- src/components/components/ComponentsContainer.js
+++ src/components/components/ComponentsContainer.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import AddComponent from './AddComponent';
-import Component from './Component';
 import CommonComponents from './CommonComponents';
+import Component from './Component';
 import DEFAULT_COMPONENTS from './DefaultComponents';
-
+import PropTypes from 'prop-types';
+import React from 'react';
 export default class ComponentsContainer extends React.Component {
   static propTypes = {
     entity: PropTypes.object
@@ -17,19 +16,20 @@ export default class ComponentsContainer extends React.Component {
   render() {
     const entity = this.props.entity;
     const components = entity ? entity.components : {};
-    const definedComponents = Object.keys(components).filter(function (key) {
+    const definedComponents = Object.keys(components).filter((key) => {
       return DEFAULT_COMPONENTS.indexOf(key) === -1;
     });
-
-    const renderedComponents = definedComponents.sort().map(function (key) {
+    const renderedComponents = definedComponents.sort().map((key, idx) => {
       return (
-        <Component
-          isCollapsed={definedComponents.length > 2}
-          component={components[key]}
-          entity={entity}
-          key={key}
-          name={key}
-        />
+        <div key={key} className={'details'}>
+          <Component
+            isCollapsed={definedComponents.length > 2}
+            component={components[key]}
+            entity={entity}
+            key={key}
+            name={key}
+          />
+        </div>
       );
     });
 
