82572c24587f6d5d0037241092703b0b50d669ba
Alexander Goryushkin <aleotu@gmail.com>
2023-07-19 15:28:56 -0400

    fix to work as described in v1, v2

--- src/lib/EditorControls.js
+++ src/lib/EditorControls.js
@@ -215,14 +215,13 @@ THREE.EditorControls = function (_object, domElement) {
   };
 
   // mouse
+  var changeStateKey = 'shiftKey';
 
   function onMouseDown(event) {
     if (scope.enabled === false) return;
 
-    const keyChangeState = event.shiftKey;
-
     if (event.button === 0) {
-      state = keyChangeState ? STATE.ROTATE : STATE.PAN;
+      state = event[changeStateKey] ? STATE.ROTATE : STATE.PAN;
     } else if (event.button === 1) {
       state = STATE.ZOOM;
     } else if (event.button === 2) {
@@ -239,6 +238,10 @@ THREE.EditorControls = function (_object, domElement) {
 
   function onMouseMove(event) {
     if (scope.enabled === false) return;
+
+    if (event.buttons === 1) {
+      state = event[changeStateKey] ? STATE.ROTATE : STATE.PAN;
+    }
 
     pointer.set(event.clientX, event.clientY);
 
