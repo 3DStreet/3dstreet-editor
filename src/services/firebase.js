d6d06f085c4f2f070f41eeec33250e1d2cf9c526
Rostyslav <rostyslav.nahornyi@zade.agency>
2023-08-20 09:57:32 +0300

    feat: environments

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,14 +4,14 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA1A6Ms9_nRsCIaxEFbmpnXMWtiVYwQOzo',
-  authDomain: 'threedstreets.firebaseapp.com',
-  databaseURL: 'https://threedstreets-default-rtdb.firebaseio.com',
-  projectId: 'threedstreets',
-  storageBucket: 'threedstreets.appspot.com',
-  messagingSenderId: '371199528858',
-  appId: '1:371199528858:web:9492578f267e12cabe3423',
-  measurementId: 'G-10ZP8K1D7R'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
