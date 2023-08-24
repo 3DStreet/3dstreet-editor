089fc4c134d79480dcafb785ac4df1b333aea5f0
Kieran Farr <kieran.farr@gmail.com>
2023-08-24 09:16:26 -0700

    remove databaseURL
    supposedly not needed in newer firebase versions

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,7 +6,6 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
