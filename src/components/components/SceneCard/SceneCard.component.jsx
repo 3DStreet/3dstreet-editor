07f2934e45efb273d628de4e41eef1840f7a51ce
Kieran Farr <kieran.farr@gmail.com>
2023-09-21 12:00:23 -0700

    add last modified timeago to scene cards

--- src/components/components/SceneCard/SceneCard.component.jsx
+++ src/components/components/SceneCard/SceneCard.component.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ScenePlaceholder from '../../../../assets/scene.png';
 import styles from './SceneCard.module.scss';
+import { formatDistanceToNow } from 'date-fns';
+
+function LastModified({ timestamp }) {
+  // Convert Firestore Timestamp to JavaScript Date object
+  const date = timestamp.toDate();
+
+  // Use date-fns to get "time ago" format
+  const timeAgo = formatDistanceToNow(date, { addSuffix: true });
+
+  return <p className={styles.date}>Last modified {timeAgo}</p>;
+}
 
 const SceneCard = ({ scenesData, handleSceneClick }) => (
   <div className={styles.wrapper}>
@@ -13,9 +24,9 @@ const SceneCard = ({ scenesData, handleSceneClick }) => (
       >
         <img src={ScenePlaceholder} alt="scene" className={styles.img} />
         <p className={styles.title}>{scene.data().title}</p>
-        {/* <p className={styles.date}>
-          Last modified 2 hours ago
-        </p> */}
+        <p className={styles.date}>
+          <LastModified timestamp={scene.data().updateTimestamp} />
+        </p>
       </div>
     ))}
   </div>
