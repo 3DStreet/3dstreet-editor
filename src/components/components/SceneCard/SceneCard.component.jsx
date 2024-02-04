9160e25f7b194190a1822476d5da56675c62b1cf
Kieran Farr <kieran.farr@gmail.com>
2024-02-04 12:05:04 -0800

    remove scene.id debug

--- src/components/components/SceneCard/SceneCard.component.jsx
+++ src/components/components/SceneCard/SceneCard.component.jsx
@@ -153,7 +153,6 @@ const SceneCard = ({
               backgroundPosition: 'center'
             }}
           />
-          {JSON.stringify({ scene: scene.id })}
           {showMenu === index && (
             <div className={styles.menuBlock}>
               <div
